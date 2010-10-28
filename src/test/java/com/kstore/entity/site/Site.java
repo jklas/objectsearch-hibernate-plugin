1d815db1d724c0f3debfd80806f6b4894225d76c
jklas <julian.klas@mercadolibre.com>
2010-10-28 14:11:09 -0300

    Revert "Added LGPL license"
    This reverts commit 7163927354f892816325565853e930699a007563.

--- src/test/java/com/kstore/entity/site/Site.java
+++ src/test/java/com/kstore/entity/site/Site.java
@@ -1,66 +1,26 @@
-/**
- * Object Search Framework
- *
- * Copyright (C) 2010 Julian Klas
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- */
 package com.kstore.entity.site;
 
-public class SiteCurrency {
+
+public class Site {	
 	
-	private Long id;
+	private String siteID;
 	
-	private Site site; 
+	Site(){}
 	
-	private String shortName;
-	
-	private float conversionValue;
-	
-	SiteCurrency() {}
-
-	public void setId(Long id) {
-		this.id = id;
+	public Site(String siteID) {
+		this.siteID = siteID;
 	}
 
-	public Long getId() {
-		return id;
+	public void setSiteID(String name) {
+		this.siteID = name;
 	}
 
-	public void setSite(Site site) {
-		this.site = site;
-	}
-
-	public Site getSite() {
-		return site;
-	}
-
-	public void setShortName(String shortName) {
-		this.shortName = shortName;
-	}
-
-	public String getShortName() {
-		return shortName;
-	}
-
-	public void setConversionValue(float conversionValue) {
-		this.conversionValue = conversionValue;
-	}
-
-	public float getConversionValue() {
-		return conversionValue;
+	public String getSiteID() {
+		return siteID;
 	}
 	
+	@Override
+	public String toString() {	
+		return siteID;
+	}
 }
